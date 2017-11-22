847c2b6873aec6071d1020a923de2a42b1e8ff58
Nick Arocho <nick@boldlyfine.com>
2017-11-21 17:04:36 -0800

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,18 +1,48 @@
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+	{
+	rank: "queen",
+	suit: "hearts",
+	cardImage: "images/queen-of-hearts.png"
+	},
+	{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png"
+	},
+	{
+	rank: "king",
+	suit: "hearts",
+	cardImage: "images/king-of-hearts.png"
+	},
+	{
+	rank: "king",
+	suit: "hearts",
+	cardImage: "images/king-of-diamonds.png"
+	},
+];
+
 var cardsInPlay = [];
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-var cardTwo = cards[2];
-cardsInPlay.push(cardTwo)
 
-
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
-
-if (cardsInPlay.length === 2) {
-	if (cardsInPlay[0] === cardsInPlay[1]) {
-		alert("You found a match!")
-	} else {
-		alert ("Sorry, try again.")
+var checkForMatch = function () {
+	if (cardsInPlay.length === 2) {
+		if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+		} else {
+		alert("Sorry, try again.");
+		}
 	}
+};
+
+var flipCard = function (cardId) {
+	console.log("User flipped " + cards[cardId].rank);
+
+	cardsInPlay.push(cards[cardId].rank);
+
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
+	
+	checkForMatch();
 }
+
+flipCard(0);
+flipCard(2);
